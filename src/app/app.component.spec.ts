8b9353b314c74088a9654aef859c40fb6e65b382
jkumar15 <jkumar15@myseneca.ca>
2018-11-30 19:37:42 -0500

    updatessss

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assignment5'`, () => {
+  it(`should have as title 'assignment6'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('assignment5');
+    expect(app.title).toEqual('assignment6');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to assignment5!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to assignment6!');
   });
 });
